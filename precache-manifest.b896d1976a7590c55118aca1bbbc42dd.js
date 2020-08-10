ece55630c4ed64537943d9d554a4de6d2ff7196b
Suban <subanosman1@gmail.com>
2020-08-10 16:46:21 -0400

    Updates

---
+++ precache-manifest.b896d1976a7590c55118aca1bbbc42dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1931f4f64062d8f732c95cdbd819285",
+    "url": "/SubanOsmanProjectFive/index.html"
+  },
+  {
+    "revision": "2cc96674c591d5d3a92d",
+    "url": "/SubanOsmanProjectFive/static/css/main.8bad12f4.chunk.css"
+  },
+  {
+    "revision": "4affc1b0aaf02ae58008",
+    "url": "/SubanOsmanProjectFive/static/js/2.5dcd5460.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/SubanOsmanProjectFive/static/js/2.5dcd5460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc96674c591d5d3a92d",
+    "url": "/SubanOsmanProjectFive/static/js/main.d676e297.chunk.js"
+  },
+  {
+    "revision": "e31ebe7c01563610733c",
+    "url": "/SubanOsmanProjectFive/static/js/runtime-main.aca036f2.js"
+  }
+]);
