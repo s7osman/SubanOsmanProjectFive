15a8c4eb239ce9984512799e81ddf5c58aae4639
Suban <subanosman1@gmail.com>
2020-08-14 01:35:36 -0400

    error handling added  :boom: :boom: :boom:

--- src/Form.js
+++ src/Form.js
@@ -10,36 +10,46 @@ class Form extends Component {
         }
     }
 
-    //When user inputs goal, save state
-    handleChange = (event) => {
+//When user inputs goal, save state
+handleChange = (event) => {
     this.setState({
         userGoal: event.target.value
     })
 }
 
+
 //when user adds goal, store value in firebase and display new goal on the screen
+// Handle Invalid Error
 handleClick = (event) => {
     event.preventDefault();
-
-    const dbRef = firebase.database().ref();
-    dbRef.push(this.state.userGoal);
-
     this.setState({
     userGoal: ''
     })
+
+    if (this.state.userGoal === '') {
+        alert ("Nice Try! Please add a Goal");
+    } else {
+        const dbRef = firebase.database().ref();
+        dbRef.push(this.state.userGoal);
+
+    }
 }
+
+
+
 
 //form to be displayed on the screen
 render() {
     return(
-        <form action="submit">
-            <label htmlFor="newGoal" className="sr-only">Add New Goal Here</label>
+        <form action="submit" >
+            <label htmlFor="newGoal" className="srOnly">Add New Goal Here</label>
             <input 
             type="text"
             id="newGoal"
             onChange={this.handleChange}
             value={this.state.userGoal}
             placeholder="Add New Goal Here"
+            tabindex="1"
             />
             <button onClick={this.handleClick}>Add Goal</button>
         </form>
